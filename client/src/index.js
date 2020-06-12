634b284ededb61096bffad9461943501dc129c5f
Walker, Corey <corey_walker@mcafee.com>
2020-06-11 20:42:26 -0700

    update App.js and add new node_modules

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppContainer from "./components/AppContainer";
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
