bbfba4e912ad7557adf2d67f760b5ca7336fd285
Lauren Doss <doss.laurene@gmail.com>
2020-06-23 21:31:19 -0700

    updated to just date picker

--- client/src/components/DateTimePicker/DTP.js
+++ client/src/components/DateTimePicker/DTP.js
@@ -1,16 +1,18 @@
 import React  from 'react';
-import DateTimePicker from 'react-datetime-picker';
+import DatePicker from 'react-date-picker';
  
 function DTP (props) {
 const min = new Date (2020, 5, 25, 12, 30)
 const max = new Date (2020, 6, 10, 12, 30)
     return (
       <div>
-        <DateTimePicker
+        <DatePicker
           onChange={props.onChange} 
           value={props.value}
           minDate = {min}
           maxDate = {max}
+         
+          
           
         />
       </div>
