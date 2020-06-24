06b05ed7bd9026ac21e7685f10c447755fb12607
Walker, Corey <corey_walker@mcafee.com>
2020-06-23 19:54:32 -0700

    make sure the state also has a default of client for signup

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -10,7 +10,7 @@ class Signup extends Component {
     email: "",
     password: "",
     passwordConfirm: "",
-    user_type: "",
+    user_type: "client",
     unMatchPwdErr: "",
     signupError: {}
   };
