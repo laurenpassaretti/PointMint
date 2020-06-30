873609bf05329ae7a7bfdfcdbc6057e9d8835031
Lauren Doss <doss.laurene@gmail.com>
2020-06-29 21:26:04 -0700

    updated client.js

--- client/src/pages/Client.js
+++ client/src/pages/Client.js
@@ -1,59 +1,52 @@
 import React, { useState, useEffect } from "react";
-import { Card, Form, Button} from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import API from "../utils/API";
 import DTP from '../components/DatePicker/DatePicker'
 import { UserContext } from "../utils/UserContext";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Alert from 'react-bootstrap/Alert'
 import './client.css';
 
 
 function Client() {
-    
-
     const { state } = React.useContext(UserContext);
-    const [ setAppointments ] = useState([])
-    const [formObject,setFormObject] = useState({
+    const [setAppointments] = useState([])
+    const [formObject, setFormObject] = useState({
         name: "",
         phone: "",
-        notes: "", 
+        notes: "",
         date: ""
     })
-    
     useEffect(() => {
         loadAppointments()
     }, [])
-
     const onChange = date => setFormObject({ ...formObject, date });
-
-    function loadAppointments(){
+    function loadAppointments() {
         API.getAppointments()
-        .then(res => setAppointments(res.data)
-        ).catch(err => console.log(err));
+            .then(res => setAppointments(res.data)
+            ).catch(err => console.log(err));
     }
-    
-    
-        const [show, setShow] = useState(false);
-      
-        if (show) {
-            return (
+    const [show, setShow] = useState(false);
+
+    if (show) {
+        return (
             <Alert variant="success" onClose={() => setShow(false)} dismissible>
-              <Alert.Heading>Your Appointment Request was Successfully Submitted!</Alert.Heading>
-            </Alert> 
-            )}; 
-        
-      
-   function showAlert() {
-       
-       setShow(true)
+                <Alert.Heading>Your Appointment Request was Successfully Submitted!</Alert.Heading>
+            </Alert>
+        )
+    };
+
+
+    function showAlert() {
+
+        setShow(true)
 
     }
-
-    function handleFormSubmit(event){
-        event.preventDefault(); 
-        if (formObject.date || formObject.notes){
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        console.log(state)
+        if (formObject.date || formObject.notes) {
             API.saveAppointment({
                 name: state.name,
                 email: state.email,
@@ -61,91 +54,79 @@ function Client() {
                 notes: formObject.notes,
                 date: formObject.date
             })
-            .then(() => setFormObject({
-                name: "",
-                phone: "", 
-                notes: "", 
-                date: ""
-            }))
-            .then(() => {
-                loadAppointments(); 
-                showAlert(); 
-            })
-            
-            .catch(err => console.log(err)); 
+                .then(() => setFormObject({
+                    name: "",
+                    phone: "",
+                    notes: "",
+                    date: ""
+                }))
+                .then(() => {
+                    loadAppointments();
+                    showAlert();
+                })
+                .catch(err => console.log(err));
         }
-        
-        
-    }   
-
-    
-    
-    
-    function handleInputChange(event){
-        const { name, value } = event.target; 
-        setFormObject({...formObject, [name]:value})
     }
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormObject({ ...formObject, [name]: value })
+    }
+    return (
+        <Container>
+            <Row>
+                <Col></Col>
+                <Col xs={12} md={8}>
+                    <div className="justify-content-md-center pt-3" id="Header">
+                        <h3 id="Header">Schedule An Appointment</h3>
+                    </div>
+                    <Form >
 
-
-
-    return(
-
-
-<Container>
-  <Row>
-    <Col></Col>
-    <Col xs={12} md={8}>
-    <Card>
-            <Card.Header>Schedule An Appointment</Card.Header>
-            <Form>
-                <Form.Group>
-                    <Form.Label>Appointment Notes</Form.Label>
-                    <Form.Control 
-                    as="textarea" 
-                    name="notes"
-                    onChange={handleInputChange}
-                    value={formObject.notes}
-                    ></Form.Control>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Enter Your Phone Number</Form.Label>
-                    <Form.Control 
-                    as="textarea" 
-                    name="number"
-                    onChange ={handleInputChange}
-                    ></Form.Control>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Available Appointments</Form.Label>
-                   
-                   <DTP
-                   onChange={onChange}
-                   value={formObject.date}
-                   name="date"
-                    />
-                    </Form.Group>
-                <Button variant="primary" type="submit"
-                onClick={handleFormSubmit}
-                id="SubmitButton"
-                >
-                    Submit
+                        <Form.Group>
+                            <div id="formtext" className="pl-3">
+                                Appointment Notes
+                    </div>
+                            <Form.Control
+                                as="textarea"
+                                name="notes"
+                                onChange={handleInputChange}
+                                value={formObject.notes}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <div id="formtext" className="pl-3">
+                                Phone Number
+                    </div>
+                            <Form.Control
+                                as="textarea"
+                                name="number"
+                                onChange={handleInputChange}
+                                value={formObject.number}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <div id="formtext" className="pl-3">
+                                Available Appointments
+                    </div>
+                            <DTP
+                                onChange={onChange}
+                                value={formObject.date}
+                                name="date"
+                            />
+                        </Form.Group>
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            onClick={handleFormSubmit}
+                            id="SubmitButton"
+                        >
+                            Submit
                 </Button>
-                
-            </Form>
-        </Card>    
-
-
-
-
-    </Col>
-    <Col></Col>
-  </Row>
-</Container>
-
-
-     
-           
+                        <div className="mb-3"></div>
+                    </Form>
+                </Col>
+                <Col></Col>
+            </Row>
+        </Container>
     );
 }
-
 export default Client;
